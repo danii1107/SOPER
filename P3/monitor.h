ce43e4af956725fd474f4b878d84f8f38a790c20
danii1107 <99350151+danii1107@users.noreply.github.com>
2023-04-10 11:11:29 +0200

    Create monitor.h

---
+++ P3/monitor.h
@@ -0,0 +1,21 @@
+#ifndef MONITOR_H
+#define MONITOR_H
+
+#include <fcntl.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/sem.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define SEM_NAME_MUTEX ".mutex"
+#define SEM_NAME_WAIT ".wait"
+#define SHM_MON_COMPR "/shm_monitor_comprobante"
+
+#endif
