81abd585b2d2567cd2fd2872475595bd164515db
danii1107 <99350151+danii1107@users.noreply.github.com>
2023-04-11 11:34:11 +0200

    Update monitor.h

--- P3/monitor.h
+++ P3/monitor.h
@@ -1,21 +1,33 @@
 #ifndef MONITOR_H
 #define MONITOR_H
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <semaphore.h>
 #include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/sem.h>
-#include <unistd.h>
-#include <errno.h>
 
-#define SEM_NAME_MUTEX ".mutex"
-#define SEM_NAME_WAIT ".wait"
-#define SHM_MON_COMPR "/shm_monitor_comprobante"
+#define SEM_NAME_MUTEX ".mutex"                     /* NOMBRE DEL ARCHIVO TEMPORAL PARA EL SEMÁFORO DE EXCLUSIÓN MUTUA */
+#define SHM_MON_COMPR "/shm_monitor_comprobante"    /* NOMBRE DEL SEGMENTO DE MEMORIA COMPARTIDA ENTRE COMPROBADOR-MONITOR */
+#define MQ_SIZE 6                                   /* TAMAÑO DE LA COLA DE MENSAJES */
+
+typedef struct {
+    long target;
+    long solution;
+} ShmData;
+
+typedef struct {
+    ShmData data[MQ_SIZE];
+    sem_t sem_mutex;
+    sem_t sem_empty;
+    sem_t sem_fill;
+} CompleteShMem;
 
 #endif
