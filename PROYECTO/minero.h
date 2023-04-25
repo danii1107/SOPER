69dc86268aa1834e95239aa87b6f440ba69bb4c4
danii1107 <99350151+danii1107@users.noreply.github.com>
2023-04-25 15:45:29 +0200

    Update minero.h

--- PROYECTO/minero.h
+++ PROYECTO/minero.h
@@ -2,6 +2,9 @@
 #define MINERO_H
 
 #define INITIAL_TARGET 0
+#define SEM_NAME_MINER_COUNT ".minerCount_mutex"
+#define SEM_NAME_SHM_CREATION ".shmCreation_mutex"
+#define SHM_NAME_MINERS "./shm_miners"
 
 typedef struct {
     long pow_ini;
