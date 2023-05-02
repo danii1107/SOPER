82dfab77ed34d5f3853c257cc329bd4700a94902
danii1107 <99350151+danii1107@users.noreply.github.com>
2023-05-02 14:06:37 +0200

    Update minero.h

--- PROYECTO/minero.h
+++ PROYECTO/minero.h
@@ -1,8 +1,10 @@
 #ifndef MINERO_H
 #define MINERO_H
 
+#define MAX_MINER 100
 #define INITIAL_TARGET 0
 #define SEM_NAME_SHM_CREATION ".shmCreation_mutex"
+#define SEM_NAME_SEND_SIGNAL ".shmSignal_mutex"
 #define SHM_NAME_MINERS "./shm_miners"
 
 typedef struct {
