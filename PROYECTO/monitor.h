ec9d13fed23b0e7f4e951cbdb29aea212c66c341
danii1107 <99350151+danii1107@users.noreply.github.com>
2023-05-02 14:05:49 +0200

    Update monitor.h

--- PROYECTO/monitor.h
+++ PROYECTO/monitor.h
@@ -3,11 +3,13 @@
 
 #include "types.h"
 
+#define SHM_MON_COMPR "./shm_monitor_comprobador"
 #define BUFF_SIZE 5
 
 typedef struct {
     int front;                       /*!< PRIMER ELEMENTO DE LA COLA CIRCULAR MONITOR-COMPROBADOR */
     int rear;                        /*!< ÍNDICE DE LA SIGUIENTE POSICIÓN LIBRE DE LA COLA CIRCULAR MONITOR-COMPROBADOR */
+    int solStatus;                   /* */
     InfoBlock data[BUFF_SIZE];       /*!< COLA CIRCULAR DE BUFF_SIZE ELEMENTOS (BLOQUE DEL MINERO), COMUNICA MONITOR-COMPROBADOR */
     sem_t sem_mutex;                 /*!< SEMÁFORO DE EXCLUSIÓN MUTUA ANÓNIMO ALGORITMO PRODUCTOR-CONSUMIDOR */
     sem_t sem_empty;                 /*!< SEMÁFORO DE COLA VACÍA ANÓNIMO ALGORITMO PRODUCTOR-CONSUMIDOR */
