c3dd826d49d73a46c74ddd6bd04fa478358ee15f
danii1107 <99350151+danii1107@users.noreply.github.com>
2023-05-04 21:52:46 +0200

    Update monitor.c

--- PROYECTO/monitor.c
+++ PROYECTO/monitor.c
@@ -1,13 +1,21 @@
 #include "monitor.h"
 
+static volatile sig_atomic_t got_sigint = 0;
+
+void handler_sigint(int sig)
+{
+    got_sigint = 1;
+}
+
 int main()
 {
-    int i = 0, j = 0;                          /* INDEXACIÓN */
-    int fd_shm_mon_compr;                      /* DESCRIPTOR PARA COMPROBADOR-MONITOR */
-    char solutionInfo[10] = "rejected\0";    /* */
-    CompleteShMem *info_shm = NULL;            /* EdD ALMACENA SEMÁFOROS Y COLA CIRCULAR COMPROBADOR-MONITOR */
-    mqd_t mq;                                  /* COLA DE MENSAJES MINERO-COMPROBADOR */
-    InfoBlock recieved_info;                   /* EdD PARA RECIBIR LA INFORMACIÓN ENVIADA POR MINERO A TRAVÉS DE MQ */
+    int i = 0, j = 0;                     /* INDEXACIÓN */
+    int fd_shm_mon_compr;                 /* DESCRIPTOR PARA COMPROBADOR-MONITOR */
+    char solutionInfo[10] = "rejected\0"; /* */
+    struct sigaction act;
+    CompleteShMem *info_shm = NULL; /* EdD ALMACENA SEMÁFOROS Y COLA CIRCULAR COMPROBADOR-MONITOR */
+    mqd_t mq;                       /* COLA DE MENSAJES MINERO-COMPROBADOR */
+    InfoBlock recieved_info;        /* EdD PARA RECIBIR LA INFORMACIÓN ENVIADA POR MINERO A TRAVÉS DE MQ */
     pid_t pid;
 
     fd_shm_mon_compr = shm_open(SHM_MON_COMPR, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
@@ -94,34 +102,36 @@ int main()
     else if (pid == 0)
     {
         /* MONITOR */
-        while (info_shm->data[info_shm->rear-1].target != -1 && info_shm->data[info_shm->rear-1].solution != -1 /* BLOQUE DE FINALIZACIÓN */)
+        while (info_shm->data[info_shm->rear - 1].target != END_BLOCK && info_shm->data[info_shm->rear - 1].solution != END_BLOCK /* BLOQUE DE FINALIZACIÓN */)
         {
-             /* CONSUMIDOR */
+            /* CONSUMIDOR */
             sem_wait(&info_shm->sem_fill);
             sem_wait(&info_shm->sem_mutex);
 
             /* MUESTRA POR PANTALLA EL ESTADO DE LA SOLUCIÓN OBTENIDA PARA SU CORRESPONDIENTE TARGET */
-            if (info_shm->data[info_shm->rear-1].target != -1 && info_shm->data[info_shm->rear-1].solution != -1)
+            if (info_shm->data[info_shm->rear - 1].target != END_BLOCK && info_shm->data[info_shm->rear - 1].solution != END_BLOCK)
             {
-                if (info_shm->solStatus == 1) strcpy(solutionInfo, "validated");  /* SOLUCIÓN CORRECTA */
-                else strcpy(solutionInfo, "rejected");                            /* SOLUCIÓN INCORRECTA */
+                if (info_shm->solStatus == 1)
+                    strcpy(solutionInfo, "validated"); /* SOLUCIÓN CORRECTA */
+                else
+                    strcpy(solutionInfo, "rejected"); /* SOLUCIÓN INCORRECTA */
 
                 printf("ID: %d\n", info_shm->data[info_shm->front].ID);
-                printf("Winner: %ld\n", (long) info_shm->data[info_shm->front].FstMiner);
+                printf("Winner: %ld\n", (long)info_shm->data[info_shm->front].FstMiner);
                 printf("Target: %8ld\n", info_shm->data[info_shm->front].target);
                 printf("Solution: %8ld (%s)\n", info_shm->data[info_shm->front].solution, solutionInfo);
                 printf("Votes: %d/%d\n", info_shm->data[info_shm->front].positive_n_votes, info_shm->data[info_shm->front].total_n_votes);
                 i = 0;
-                while(info_shm->data[info_shm->front].wallets[i].MinersWalletPID != (pid_t) 0)
+                while (info_shm->data[info_shm->front].wallets[i].MinersWalletPID != (pid_t)0)
                 {
-                    printf("Wallets: %ld:%ld\n", (long) info_shm->data[info_shm->front].wallets[i].MinersWalletPID, info_shm->data[info_shm->front].wallets[i].n_coins);
+                    printf("Wallets: %ld:%ld\n", (long)info_shm->data[info_shm->front].wallets[i].MinersWalletPID, info_shm->data[info_shm->front].wallets[i].n_coins);
                     i++;
                 }
             }
-            
+
             /* "POP" COLA CIRCULAR MONITOR-COMPROBADOR */
             info_shm->front = (info_shm->front + 1) % BUFF_SIZE;
-            
+
             sem_post(&info_shm->sem_mutex);
             sem_post(&info_shm->sem_empty);
         }
@@ -135,15 +145,25 @@ int main()
     {
         /* COMPROBADOR */
 
+        act.sa_handler = handler_sigint;
+        sigemptyset(&(act.sa_mask));
+        act.sa_flags = 0;
+
+        if (sigaction(SIGINT, &act, NULL) < 0)
+        {
+            perror("sigaction");
+            exit(EXIT_FAILURE);
+        }
+
         /* APERTURA DE LA MQ MINERO-COMPROBADOR, SI YA EXISTE SE ABRE SOLO PARA LEER */
-        mq = mq_open( MQ_NAME , O_CREAT | O_RDONLY, S_IRUSR | S_IWUSR, &attributes);
-        if (mq == (mqd_t) - 1)
+        mq = mq_open(MQ_NAME, O_CREAT | O_RDONLY, S_IRUSR | S_IWUSR, &attributes);
+        if (mq == (mqd_t)-1)
         {
             sem_destroy(&info_shm->sem_mutex);
             sem_destroy(&info_shm->sem_empty);
             sem_destroy(&info_shm->sem_fill);
             munmap(info_shm, sizeof(CompleteShMem));
-            shm_unlink( SHM_MON_COMPR );
+            shm_unlink(SHM_MON_COMPR);
             fprintf(stderr, "Error creating the message queue\n");
             exit(EXIT_FAILURE);
         }
@@ -151,22 +171,22 @@ int main()
         recieved_info.target = 0;
         recieved_info.solution = 0;
 
-        while(recieved_info.target != -1 && recieved_info.solution != -1 /* BLOQUE FINALIZACION DE LECT/ESCR */)
+        while ((recieved_info.target != END_BLOCK && recieved_info.solution != END_BLOCK) && got_sigint == 0 /* BLOQUE FINALIZACION DE LECT/ESCR O RECIBE SIGINT */)
         {
             /* PRODUCTOR */
             sem_wait(&info_shm->sem_empty);
             sem_wait(&info_shm->sem_mutex);
-            
+
             /* EXTRAER DE LA COLA DE MENSAJES MINERO */
-            if (mq_receive(mq, (char *) &recieved_info, sizeof(InfoBlock), NULL) == -1)
+            if (mq_receive(mq, (char *)&recieved_info, sizeof(InfoBlock), NULL) == -1)
             {
                 sem_destroy(&info_shm->sem_mutex);
                 sem_destroy(&info_shm->sem_empty);
                 sem_destroy(&info_shm->sem_fill);
                 munmap(info_shm, sizeof(CompleteShMem));
-                shm_unlink( SHM_MON_COMPR );
+                shm_unlink(SHM_MON_COMPR);
                 mq_close(mq);
-                mq_unlink( MQ_NAME );
+                mq_unlink(MQ_NAME);
                 fprintf(stderr, "Error recieving from mq\n");
                 exit(EXIT_FAILURE);
             }
@@ -174,11 +194,20 @@ int main()
             /* METER EN MEMEORIA COMPARTIDA LA INFORMACIÓN RECIBIDA DE MINERO */
             info_shm->data[info_shm->rear].target = recieved_info.target;
             info_shm->data[info_shm->rear].solution = recieved_info.solution;
-            
+
             /* COMPROBAR SOLUCIÓN; CORRECTA = 1, ERRÓNEA = 0 */
-            if (info_shm->data[info_shm->rear].target == pow_hash(info_shm->data[info_shm->rear].solution)) info_shm->solStatus = 1;
-            else info_shm->solStatus = 0;
-            
+            if (info_shm->data[info_shm->rear].target == pow_hash(info_shm->data[info_shm->rear].solution))
+                info_shm->solStatus = 1;
+            else
+                info_shm->solStatus = 0;
+
+            if (got_sigint) /* HA RECIBIDO SIGINT */
+            {
+                /* ENVIAR END_BLOCK A MONITOR */
+                info_shm->data[info_shm->rear].target = END_BLOCK;
+                info_shm->data[info_shm->rear].solution = END_BLOCK;
+            }
+
             /* "PUSH" COLA CIRCULAR MONITOR-COMPROBADOR */
             info_shm->rear = (info_shm->rear + 1) % BUFF_SIZE;
 
@@ -190,7 +219,7 @@ int main()
         sem_destroy(&info_shm->sem_mutex);
         sem_destroy(&info_shm->sem_empty);
         sem_destroy(&info_shm->sem_fill);
-        
+
         /* "LIBERACIÓN" DEL MAPEO DE LA MEMORIA DESDE COMPROBADOR */
         munmap(info_shm, sizeof(CompleteShMem));
 
